12ea4110ca23716f92749a2eb24480ebe8c7364e
aransa <aransafp@yahoo.com>
2019-10-08 10:07:56 +0700

    tambah file

--- src/Solution.java
+++ src/Solution.java
@@ -2,7 +2,11 @@ public class Solution {
     public static void main(String[] args) {
 
         PersegiPanjang bd1 = new PersegiPanjang(10, 20);
-        
+
         System.out.println(bd1.getLuas());
+
+        Lingkaran bd2 = new Lingkaran(7);
+
+        System.out.println(bd2.getLuas());
     }
 }
